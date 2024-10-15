406af041d7987b2b446b20cd47d8ae42ed793ea0
AndreyOlorin <sap7andy@gmail.com>
2024-10-15 20:18:01 +0700

    Исправления важных ошибок

--- app/src/main/java/com/example/kotlinlaba3/MainActivity.kt
+++ app/src/main/java/com/example/kotlinlaba3/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.kotlinlaba3
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,10 +19,31 @@ class MainActivity : AppCompatActivity() {
 
         val button = findViewById<Button>(R.id.startButton)
         val editString = findViewById<TextView>(R.id.editString)
-        val yourNumber = findViewById<EditText>(R.id.yourNumber)
-        val yourSymbol = findViewById<EditText>(R.id.yourSymbol)
 
         button.setOnClickListener(){
+
+            val yourNumber = findViewById<EditText>(R.id.yourNumber)
+            val yourSymbol = findViewById<EditText>(R.id.yourSymbol)
+
+            if (yourNumber.text.toString() == ""){
+                yourNumber.setText("0")
+            }
+
+            // || yourNumber.text.toString().toInt() == 0
+            if (yourNumber.text.toString().toInt() > editString.text.length  || yourNumber.text.toString() == ""){
+                Toast.makeText(this, "Число больше изменяемой строки", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                val newString  = yourSymbol.text.toString().repeat(yourNumber.text.toString().toInt())
+                editString.text = editString.text.substring(newString.length)
+                editString.text = newString + editString.text
+            }
+
+
+
+
+
+
 
         }
     }
